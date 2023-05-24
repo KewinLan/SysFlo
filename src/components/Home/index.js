83c7f944533fec88b365b6a0dee3f1adedff4051
ky-lan <113309181+ky-lan@users.noreply.github.com>
2023-05-23 22:16:29 -0400

    Home page done, maybe add a hover effect to the images. Do footer next

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -21,78 +21,39 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className='home-products-area-top'>
-          <div className='gum-images-row1'>
+        <div className='home-products-area'>
+          <div className='products-images-row1'>
             <div className="writing-utencils-img-link">
-              <Link to="/faq">
-                <img src="images/WritingUtencilsButton.png" className="banana-cherry-img" alt="Writing Utencils. Save up to 30% only until September 30." width="596" height="321" />
+              <Link to="/cart">
+                <img src="images/WritingUtencilsButton.png" className="writing-utencils-img" alt="Writing Utencils. Save up to 30% only until September 30." width="596" height="321" />
               </Link>
             </div>
-            <div className="peppermint-lemon-img-link">
-              <img src="images/Pepperm_to_Lemon1.png" className="peppermint-lemon-img" alt="Apple Strawberry Bottle" width="596" height="321" />
+            <div className="sticky-notes-img-link">
+              <Link to="/cart">
+                <img src="images/StickyNotesButton.png" className="sticky-notes-lemon-img" alt="Sticky Notes. Save up to 30% only until September 30." width="596" height="321" />
+              </Link>
             </div>
-            <div className="raspberry-peppermint-img-link">
-              <img src="images/Rasp_to_Pepperm1.png" className="raspberry-peppermint-img" alt="Mango Strawberry Bottle" width="596" height="321" />
-            </div>
-          </div>
-          <div className='names-of-gum-row1'>
-            <div className='banana-cherry-label'>
-              Banana to Cherry
-            </div>
-            <div className='peppermint-lemon-label'>
-              Peppermint to Lemon
-            </div>
-            <div className='raspberry-peppermint-label'>
-              Raspberry to Peppermint
+            <div className="elastics-img-link">
+              <Link to="/cart">
+                <img src="images/ElasticsButton.png" className="elastics-img" alt="Elastics. Save up to 30% only until September 30." width="596" height="321" />
+              </Link>
             </div>
           </div>
-          <div className='home-explore-btns1'>
-            <div className='home-explore-btn1'>
-              <button>Explore</button>
+          <div className='products-images-row2'>      
+            <div className="paper-clips-img-link">
+              <Link to="/cart">
+                <img src="images/PaperClipsButton.png" className="paper-clips-img" alt="Paper Clips. Save up to 30% only until September 30." width="596" height="321" />
+              </Link>
             </div>
-            <div className='home-explore-btn2'>
-              <button>Explore</button>
+            <div className="pins-img-link">
+              <Link to="/cart">
+                <img src="images/PinsButton.png" className="pins-img" alt="Pins. Save up to 30% only until September 30." width="596" height="321" />
+              </Link>
             </div>
-            <div className='home-explore-btn3'>
-              <button>Explore</button>
-            </div>
-          </div>
-          
-
-          <div className='home-bg'></div>
-          <div className='gum-images-row2'>      
-            <div className="cherry-lemon-img-link">
-              <img src="images/Cherry_to_Lemon1.png" className="cherry-lemon-img" alt="Apple Grape Bottle" width="596" height="321" />
-            </div>
-            <div className="banana-peppermint-img-link">
-              <img src="images/Banana_to_Pepperm1.png" className="banana-peppermint-img" alt="Grape Strawberry Bottle" width="596" height="321" />
-            </div>
-            <div className="peppermint-cherry-img-link">
-              <img src="images/Pepperm_to_Cherry1.png" className="peppermint-cherry-img" alt="Grape Strawberry Bottle" width="596" height="321" />
-            </div>
-          </div>
-          <div className='names-of-gum-row2'>
-            <div className='cherry-lemon-label'>
-              Cherry to Lemon
-            </div>
-            <div className='banana-peppermint-label'>
-              Banana to Peppermint
-            </div>
-            <div className='peppermint-cherry-label'>
-              Peppermint to Cherry
-            </div>
-          </div>
-        
-
-          <div className='home-explore-btns2'>
-            <div className='home-explore-btn4'>
-              <button>Explore</button>
-            </div>
-            <div className='home-explore-btn5'>
-              <button>Explore</button>
-            </div>
-            <div className='home-explore-btn6'>
-              <button>Explore</button>
+            <div className="binder-clips-img-link">
+              <Link to="/cart">
+                <img src="images/BinderClipsButton.png" className="binder-clips-img" alt="Binder Clips. Save up to 30% only until September 30." width="596" height="321" />
+              </Link>
             </div>
           </div>
         </div>
