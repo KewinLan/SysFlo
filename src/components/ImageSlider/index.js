9c88645e5bab8d078cfc4e323f8af11c3cd2532c
ky-lan <113309181+ky-lan@users.noreply.github.com>
2023-05-25 00:44:09 -0400

    continue cart's carousel

--- src/components/ImageSlider/index.js
+++ src/components/ImageSlider/index.js
@@ -6,11 +6,16 @@ const ImageSlider = ({slides}) => {
   const sliderStyles = {
     height: "100%",
     position: "relative",
+    top: "15.25em",
+    left: "20.5em",
   };
   const slideStyles = {
-    width: "100%",
-    height: "100%",
-    borderRadius: "10px",
+    width: "626px",
+    height: "323px",
+    padding: "1em 1em 1em 1em",
+    // position: "relative",
+    // top: "3em",
+    // borderRadius: "10px",
     backgroundPosition: "center",
     backgroundSize: "cover",
     backgroundImage: `url(${slides[currentIndex].url})`,
@@ -18,20 +23,21 @@ const ImageSlider = ({slides}) => {
   const leftArrowStyles = {
     position: "absolute",
     top: "50%",
-    transform: "translate(0, -50%)",
+    transform: "translate(-2.5em, -50%)",
     left: "32px",
     fontSize: "45px",
-    color: "#fff",
+    color: "#182030",
     zIndex: 1,
     cursor: "pointer",
   };
   const rightArrowStyles = {
     position: "absolute",
     top: "50%",
-    transform: "translate(0, -50%)",
+    transform: "translate(6em, -50%)",
+    // color: "#182030",
     right: "32px",
     fontSize: "45px",
-    color: "#fff",
+    color: "#182030",
     zIndex: 1,
     cursor: "pointer",
   };
