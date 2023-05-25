18ce63462dadd080dbe5a21cdf1a39f789e7c14c
ky-lan <113309181+ky-lan@users.noreply.github.com>
2023-05-24 23:52:06 -0400

    cart's carousel seems to work, now adjust it and add styling

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,33 +1,33 @@
 import React from 'react';
-import AuthDetails from '../auth/AuthDetails/AuthDetails';
+import ImageSlider from "../ImageSlider";
+// import AuthDetails from '../auth/AuthDetails/AuthDetails';
 import './Cart.scss';
-import SignIn from '../auth/SignIn/SignIn';
-import SignUp from '../auth/SignUp/SignUp';
+// import SignIn from '../auth/SignIn/SignIn';
+// import SignUp from '../auth/SignUp/SignUp';
 
 export default function Cart() {
+  const slides = [
+    {url: 'images/WritingUtencilsButton.png', title: 'Beach'},
+    {url: 'images/ElasticsButton.png', title: 'House'},
+    {url: 'images/PaperClipsButton.png', title: 'Boat'},
+    {url: 'images/PinsButton.png', title: 'Car'},
+    {url: 'images/StickyNotesButton.png', title: 'Road'},
+    {url: 'images/BinderClipsButton.png', title: 'Forest'}
+  ];
+
+  const containerStyles = {
+    width: "500px",
+    height: "280px",
+    margin: "0 auto",
+  };
+
   return (
     <div className='cart-everything'>
       <div className='cart-title'>
         <h1>Your Cart</h1>
-      </div>
-      <div className="account-section">
-        <div className='sign-in-section'>
-          <div className='sign-in-head2'>
-            <h2>Sign In</h2>
-          </div>
-          <br></br>
-          <SignIn />
+        <div style={containerStyles}>
+          <ImageSlider slides={slides} />
         </div>
-        <div className='sign-up-section'>
-          <div className='sign-up-head2'>
-            <h2>Sign Up</h2>
-          </div>
-          <br></br>
-          <SignUp />
-        </div>
-      </div>
-      <div className='authdetails-section'>
-        <AuthDetails />
       </div>
     </div>
   );
