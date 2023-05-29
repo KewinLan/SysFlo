f24adf52596f2b6f5137b3a2fe3907cacef80996
ky-lan <113309181+ky-lan@users.noreply.github.com>
2023-05-29 17:41:21 -0400

    faq page done, do business page

--- src/components/FAQ/index.js
+++ src/components/FAQ/index.js
@@ -6,21 +6,58 @@ export default function FAQ(props) {
   return (
     <>
       <div className='faq-title'>
-        <h1>Gum that changes flavour as you chew</h1>
+        <h1>FAQ</h1>
       </div>
-      <div className="faq-everything">
-        <div className='faq-body'>
-          <p>Gum loses its flavour after several minutes of chewing. We want to prevent that from happening. We crafted a trademarked formula that causes the flavour to change after about 10 minutes of chewing. That ways, you can live dynamically!
-          </p>
+      <div className='faq-body'>
+        <div className='faq-q1-question'>
+          <h2>Do you ship worldwide?</h2>
+          <div className='faq-q1-answer'>
+            <p>Yes, we ship worldwide.</p>
+          </div>
         </div>
-        <div className='faq-bg'></div>
-
-        <div className='-faq-btns'>
-          <Link to="/">
-            <div className='faq-premade-btn1'>
-              <button>Pre-made gum</button>
+        <div className='faq-q2-question'>
+          <h2>Do you serve businesses?</h2>
+          <div className='faq-q2-answer'>
+            <p>Yes, we serve both small and large businesses. You can see more on our </p>
+            <div className='faq-q2-answer-link'>
+              <Link to='/forbusinesses'>
+                <p>"For Businesses" </p>
+              </Link>
             </div>
+            <p>page.</p>
+          </div>
+        </div>
+        <div className='faq-q3-question'>
+          <h2>What does your company name mean?</h2>
+          <div className='faq-q3-answer'>
+            <p>The name SysFlo is a mix of the words "system" and "flow", because our office supplies allow for our customers to have a systematic way of organizing their workflow.</p>
+          </div>
+        </div>
+        <div className='faq-btn'>
+          <Link to='/'>
+            <button>Shop Now</button>
           </Link>
+        </div>
+      </div>
+      <div className='faq-bg'></div>
+
+      <div className="faq-footer">
+        <div className='faq-footer-logo'>
+          <Link to="/" className='faq-logo-text1'>
+            <img src="images/SysFloLogo.png" alt="SysFlo" className='faq-sysflo-logo-image' width="184" height="46" />
+          </Link>
+        </div>
+        <div className='faq-footer-s-right'>
+          <div className="faq-terms">
+            <Link to="/terms" className='faq-terms-link'>
+              Legal
+            </Link>
+          </div>
+          <div className="faq-privacy">
+            <Link to="/privacy" className='faq-privacy-link'>
+              Privacy
+            </Link>
+          </div>
         </div>
       </div>
     </>
